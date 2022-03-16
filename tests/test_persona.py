4f9d3a93fc562534c83636a80923a03f3f90fa6f
cristaor <c.agudeloh@uniandes.edu.co>
2022-03-15 19:55:09 -0500

    Cambiando test para probar automerge

--- tests/test_persona.py
+++ tests/test_persona.py
@@ -6,22 +6,22 @@ from sqlalchemy import text
 class PersonaTestCase(unittest.TestCase):
 
    def setUp(self):
-      self.persona1 = Persona(nombre='Alejandra', edad=25)
+      self.persona1 = Persona(nombre='Alejandro', edad=45)
       self.persona2 = Persona(nombre='Diego', edad=22)
       self.persona3 = Persona(nombre='Alejandra', edad=25)
       self.persona4 = Persona(nombre='Diana', edad=25)
-      self.persona5 = Persona(nombre='Juan', edad=15)
+      self.persona5 = Persona(nombre='Juan', edad=55)
       self.grupo = [self.persona1, self.persona2, self.persona3]
 
    def test_constructor(self):
-      self.assertEqual(self.persona1.dar_nombre(), 'Alejandra')
-      self.assertEqual(self.persona1.dar_edad(), 25)
+      self.assertEqual(self.persona1.dar_nombre(), 'Alejandro')
+      self.assertEqual(self.persona1.dar_edad(), 45)
 
    def test_anio_nacimiento(self):
-      self.assertEqual(self.persona1.calcular_anio_nacimiento(True), datetime.datetime.now().year - 25)
-      self.assertNotEqual(self.persona1.calcular_anio_nacimiento(False), datetime.datetime.now().year - 25)
-      self.assertEqual(self.persona1.calcular_anio_nacimiento(False), datetime.datetime.now().year - 25 + 1)
-      self.assertNotEqual(self.persona1.calcular_anio_nacimiento(True), datetime.datetime.now().year - 25 + 1)
+      self.assertEqual(self.persona1.calcular_anio_nacimiento(True), datetime.datetime.now().year - 45)
+      self.assertNotEqual(self.persona1.calcular_anio_nacimiento(False), datetime.datetime.now().year - 45)
+      self.assertEqual(self.persona1.calcular_anio_nacimiento(False), datetime.datetime.now().year - 45 + 1)
+      self.assertNotEqual(self.persona1.calcular_anio_nacimiento(True), datetime.datetime.now().year - 45 + 1)
 
    def test_asingacion(self):
       self.persona2.asignar_edad(28)
