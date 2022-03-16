d6fafa69a41720b6d4d0836708bb4c0bd65e86cc
cristaor <c.agudeloh@uniandes.edu.co>
2022-03-15 21:32:38 -0500

    PROBAN la nueva rama7

--- tests/test_persona.py
+++ tests/test_persona.py
@@ -6,7 +6,7 @@ from sqlalchemy import text
 class PersonaTestCase(unittest.TestCase):
 
    def setUp(self):
-      self.persona1 = Persona(nombre='Alejandro', edad=45)
+      self.persona1 = Persona(nombre='Alejandra', edad=45)
       self.persona2 = Persona(nombre='Diego', edad=22)
       self.persona3 = Persona(nombre='Alejandra', edad=25)
       self.persona4 = Persona(nombre='Diana', edad=25)
